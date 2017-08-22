08d7c3c8e0e4886c6b30f2c48a7b04a26ea76f90
anhtuanhsgs <anhtuanhsgs@gmail.com>
2017-08-22 23:49:00 +0900

    fix failure of greedy solution

--- Discrete Optimization/7W/vrp/vrp.cpp
+++ Discrete Optimization/7W/vrp/vrp.cpp
@@ -14,27 +14,37 @@ void input ();
 void init ();
 void distance_greedy_sol ();
 void process ();
-void print_sol ();
+void print_sol (); 
+void fix_failure ();
 
 struct Customer {
 	int demand, index;
 	double x, y, cost_val;
+	int picked;
 
 	Customer* l;
 	Customer* r;
 
 	Customer (){
+		picked = -1;
 		l = NULL;
 		r = NULL;
 		cost_val = -1;
 	};
 
 	double cost (){
+		if (!this->l){
+			cost_val = -1;
+			return 0;
+		}
+
 		if (cost_val < 0){
-			return dist[index][l->index] + dist[index][r->index];
+			return cost_val = dist[index][l->index] + dist[index][r->index];
 		}
 		return cost_val;
 	}
+
+	double insert (Customer *other);
 };
 
 struct Route {
@@ -67,10 +77,39 @@ ostream& operator << (ostream& os, const Customer& c){
 vector <Customer*> cus;
 vector <Route> rou;
 
+double Customer::insert(Customer *other){
+	{
+
+		double old_cost = dist[this->index][this->r->index] + other->cost ();
+		double new_cost = 0;
+		if (other->l != NULL){
+
+			other->l->r = other->r;
+			other->r->l = other->l;
+
+			new_cost += dist[other->l->index][other->r->index];
+			rou[other->picked].cap -= other->demand;
+			rou[other->picked].cost -= other->cost ();
+		}
+		rou[this->picked].cost -= dist[index][r->index];
+		other->r = this->r;
+		other->l = this;
+		this->r = other;
+		other->picked = this->picked;
+		rou[this->picked].cap += other->demand;
+		rou[this->picked].cost += other->cost ();
+
+		new_cost += dist[this->index][other->index];
+		
+		return new_cost - old_cost;
+	}
+}
+
 int main (){
 	input ();
 	init ();
 	process ();
+
 	print_sol ();
 }
 
@@ -99,11 +138,14 @@ void init (){
 	rou.assign (m, Route (cus[0]->x, cus[0]->y));
 	for (int i = 0; i < m; i++){
 		rou[i] = Route (cus[0]->x, cus[0]->y);
+		rou[i].start->picked = i;
+		cus.push_back (rou[i].start);
 	}
 }
 
 void process (){
 	distance_greedy_sol ();
+	fix_failure ();
 }
 
 void distance_greedy_sol (){
@@ -114,15 +156,13 @@ void distance_greedy_sol (){
 	while (tmp--){
 		double mcost = oo;
 		Customer* best_pos, *best_cus;
-		int car = -1;
 		for (int i = 1; i < n; i++){
-			if (picked[i]) continue;
+			if (cus[i]->picked >= 0) continue;
 			for (int j = 0; j < m; j++){
 				if (rou[j].cap + cus[i]->demand <= capacity){
 					Customer *cur = rou[j].start;
 
 					while (cur != rou[j].end){
-						// cerr << cur->index << endl;
 						double cost = dist[cur->index][cus[i]->index] + dist[cur->r->index][cus[i]->index]
 									- dist[cur->index][cur->r->index];
 
@@ -130,7 +170,6 @@ void distance_greedy_sol (){
 							mcost = cost;
 							best_pos = cur;
 							best_cus = cus[i];
-							car = j;
 						}
 						cur = cur->r;
 					}
@@ -142,26 +181,43 @@ void distance_greedy_sol (){
 			// cerr << "Fuk!!!! " << tmp << endl;  
 		}
 		else{
-			best_cus->r = best_pos->r;
-			best_cus->l = best_pos;
-			best_pos->r->l = best_cus;
-			best_pos->r = best_cus;
-			picked[best_cus->index] = 1;
-			rou[car].cap += best_cus->demand;
-			rou[car].cost += mcost;
-
-			// cerr << best_cus->l->index << ' ' << best_cus->index << ' ' << best_cus->r->index << endl;
+			best_pos->insert (best_cus);
 		}
 	}
 
 	// double cc = 0;
-	for (int i = 1; i < n; i++){
-		if (!picked[i]) cerr << "Fukkk " << i << endl;
-	}
-	for (int i = 0; i < m; i++){
-		cerr << rou[i].cap << endl;
-	}
+	// for (int i = 1; i < n; i++){
+	// 	if (cus[i]->picked < 0) cerr << "Fukkk " << i << endl;
+	// }
+	// for (int i = 0; i < m; i++){
+	// 	cerr << rou[i].cap << endl;
+	// }
 	// cerr << cc;
+}
+
+void fix_failure (){
+	// Overload
+	for (int i = 1; i < n; i++){
+		if (cus[i]->picked < 0){
+			double mcost = oo;
+			Customer *best_pos;
+			for (int j = 1; j < cus.size (); j++){
+				if (cus[j]->picked < 0) continue;
+				double cost = dist[cus[i]->index][cus[j]->index] + dist[cus[i]->index][cus[j]->r->index]
+							 - dist[cus[j]->index][cus[j]->r->index];
+
+				if (cost < mcost){
+					mcost = cost;
+					best_pos = cus[j];
+				}
+			}
+
+			best_pos->insert (cus[i]);
+		}
+	}
+
+	// Fix overload
+	
 }
 
 void print_sol (){
